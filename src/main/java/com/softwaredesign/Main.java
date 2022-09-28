8cc88c2ec8fcae17631fbfe2d61dd600bc43ff3d
Atharva Rajurkar <asrajurk@cougarnet.uh.edu>
2022-09-28 11:14:08 -0500

    Added sample junit test.

--- src/main/java/com/softwaredesign/Main.java
+++ src/main/java/com/softwaredesign/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public int sum(int a, int b) {
+        return a+b;
+    }
 }
