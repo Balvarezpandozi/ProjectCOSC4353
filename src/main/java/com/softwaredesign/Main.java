88e984378d9303dd5063f5cec318cd1b9901f30e
Bryan <bdap121299@gmail.com>
2022-09-28 11:17:57 -0500

    Added test for network interface handler

--- src/main/java/com/softwaredesign/Main.java
+++ src/main/java/com/softwaredesign/Main.java
@@ -5,10 +5,17 @@ import org.pcap4j.core.NotOpenException;
 import org.pcap4j.core.PcapNativeException;
 import org.pcap4j.core.PcapNetworkInterface;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        NetworkInterfaceHandler networkInterfaceHandler = new NetworkInterfaceHandler();
+        try {
+            System.out.println(networkInterfaceHandler.getAllDevices().get(0));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
