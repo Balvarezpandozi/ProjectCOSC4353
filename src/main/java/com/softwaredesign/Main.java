a9824d887ce7b4e9ab509e60be80bb0f7204f22a
Atharva Rajurkar <asrajurk@cougarnet.uh.edu>
2022-09-28 18:54:06 -0500

    Deleted junit test class for Main.java .

--- src/main/java/com/softwaredesign/Main.java
+++ src/main/java/com/softwaredesign/Main.java
@@ -1,14 +1,8 @@
 package com.softwaredesign;
 
 
-import org.pcap4j.core.NotOpenException;
-import org.pcap4j.core.PcapNativeException;
-import org.pcap4j.core.PcapNetworkInterface;
-
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-
+    System.out.println("Hello world");
     }
 }
