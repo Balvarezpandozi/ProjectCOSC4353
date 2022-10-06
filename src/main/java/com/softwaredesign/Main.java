db2d6fa49214b06bb9a79571d4b15ae5da8dd260
Bryan <bdap121299@gmail.com>
2022-10-05 20:01:05 -0500

    Writing pcap file functionality

--- src/main/java/com/softwaredesign/Main.java
+++ src/main/java/com/softwaredesign/Main.java
@@ -1,11 +1,10 @@
 package com.softwaredesign;
 
-
 import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PcapHandle;
 import org.pcap4j.core.PcapNativeException;
-import org.pcap4j.core.PcapNetworkInterface;
 
-import java.util.List;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
