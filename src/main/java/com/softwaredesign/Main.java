690c10ac1ed148f47324f6186c12118eb31a1efc
Atharva Rajurkar <asrajurk@cougarnet.uh.edu>
2022-10-18 17:55:04 -0500

    Code refactored.

--- src/main/java/com/softwaredesign/Main.java
+++ src/main/java/com/softwaredesign/Main.java
@@ -2,12 +2,6 @@ package com.softwaredesign;
 
 public class Main {
     public static void main(String[] args) {
-        NetworkInterfaceHandler networkInterfaceHandler = new NetworkInterfaceHandler();
-        try {
-            networkInterfaceHandler.sniff(2, 10000);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 
 }
